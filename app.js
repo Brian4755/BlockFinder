91e0bad4bd6505e33f595d4ec81e8fcdaadf207e
Brian4755 <brian.mccune1@yahoo.com>
2022-11-24 12:42:17 -0600

    notes for project;

---
+++ app.js
@@ -0,0 +1,11 @@
+// Link 20 by 20 grid with array index?
+
+
+
+// Have an initial selected spot on grid
+
+// Have goal block on grid
+
+// Relate selected spot and array goal block with array index / number assign
+
+// Selected spot move towards goal block on grid
