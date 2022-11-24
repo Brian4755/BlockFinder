4b302364da25da89ccbd2f4b565e2a9143c228bf
Brian4755 <brian.mccune1@yahoo.com>
2022-11-24 13:59:20 -0600

    small board with render block and goal

--- app.js
+++ app.js
@@ -9,3 +9,41 @@
 // Relate selected spot and array goal block with array index / number assign
 
 // Selected spot move towards goal block on grid
+let blockSelect = document.querySelectorAll('.block')
+
+
+let block
+let goal
+let grid
+
+function init() {
+  grid = [
+    null, null, null, null, null,
+    null, null, null, null, null,
+    null, null, null, null, null,
+    null, null, null, null, null,
+    null, null, null, null, null,
+  ]
+   block = Math.floor(Math.random()*24)
+   goal = Math.floor(Math.random()*24)
+
+   grid[block] = 1
+   grid[goal] = 2
+   render()
+}
+
+
+function render() {
+for (let i=0; i<grid.length; i++) {
+    if (grid[i] === 1) {
+      blockSelect[i].textContent = 'B'
+    }
+    if (grid[i] === 2) {
+      blockSelect[i].textContent = 'G'
+    }
+  }
+}
+
+
+init()
+console.log(grid)
